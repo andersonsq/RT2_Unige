0237cc01e487bdaf7c3de304b3b8147ea7cf3a94
andersonsq <andersonsq.andrade@gmail.com>
2021-08-06 17:31:40 +0200

    removed a wrong file

--- conf.py
+++ conf.py
@@ -42,7 +42,7 @@ extensions = [
   'sphinx.ext.ifconfig',
   'sphinx.ext.viewcode',
   'sphinx.ext.githubpages',
-  "sphinx.ext.napoleon",
+  'sphinx.ext.napoleon',
   'sphinx.ext.inheritance_diagram',
   'breathe'
 ]
